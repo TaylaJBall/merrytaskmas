be971b4ef513f170e88d66044624d12420803287
Tayla Ball <tayla.ball@hotmail.com>
2024-12-16 12:33:56 +0000

    Add: All auth and started the GiftList model

--- giftlist/models.py
+++ giftlist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class GiftList(models.Model):
+    """
+    A model to create and manage giftlists
+    """
+    giftlistID = models.AutoField(primary_key=True)
+    userID = models.ForeignKey(User, related_name='giftlist_user', on_delete=models.CASCADE)
+    
