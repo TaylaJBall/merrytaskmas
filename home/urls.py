7f0e5ae72c3c7f68987343fe70c63c4686c1dd0e
Laura Hall <laura@allthehalls.com>
2024-12-16 17:20:17 +0000

    Add: correct redirect for landing page, styling for accounts pages

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,14 @@
 from . import views
+from .views import HomePage 
+from django.shortcuts import redirect
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 # Create your views here.
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    # Root URL: Redirect unauthenticated users to login, authenticated users to index
+    path('', lambda request: redirect('/index/') if request.user.is_authenticated else redirect('account_login'), name='home'),
+    # Define the index page (for authenticated users)
+    path('index/', HomePage.as_view(), name='index'),
 ]
