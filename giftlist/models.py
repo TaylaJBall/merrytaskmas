faaef8a55d424ba792871e03106e36f1024a3205
Tayla Ball <tayla.ball@hotmail.com>
2024-12-16 12:49:46 +0000

    Add: Items model to giftlist models.py and completed the giftlist model

--- giftlist/models.py
+++ giftlist/models.py
@@ -7,6 +7,21 @@ class GiftList(models.Model):
     """
     A model to create and manage giftlists
     """
-    giftlistID = models.AutoField(primary_key=True)
-    userID = models.ForeignKey(User, related_name='giftlist_user', on_delete=models.CASCADE)
-    
+    giftlist_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, related_name='giftlist_user', on_delete=models.CASCADE)
+    person_name = models.CharField(max_length=200)
+    budget = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Items(models.Model):
+    """
+    A model to create Items to add to giftlist
+    """
+    item_id = models.AutoField(primary_key=True)
+    giftlist_id = models.ForeignKey(GiftList, related_name='giftlist', on_delete=models.CASCADE)
+    description = models.CharField(max_length=500, null=False, blank=False)
+    link = models.CharField(max_length=500, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
