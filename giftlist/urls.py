7fc9065ae0b5c5b29c1fd1110b5ee39cbb05fece
Tayla Ball <tayla.ball@hotmail.com>
2024-12-16 17:00:25 +0000

    Add: add_item.html and linked up with urls, added the views to add an item and a gift list

--- giftlist/urls.py
+++ giftlist/urls.py
@@ -4,4 +4,5 @@ from .views import GiftList, AddGiftList
 urlpatterns = [
     path('', GiftList.as_view(), name='giftlist'),
     path('add_giftlist/', AddGiftList.as_view(), name='add_giftlist'),
+    path('add_item/', AddItem.as_view(), name='add_item'),
 ]
--- giftlist/views.py
+++ giftlist/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView, CreateView
-from .models import GiftList
+from .models import GiftList, Item
 from .forms import GiftListForm, ItemForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class GiftList(TemplateView):
     template_name = 'giftlist/giftlist.html'
 
-class AddGiftList(CreateView):
+class AddGiftList(LoginRequiredMixin ,CreateView):
     """
-    Add Giftlist
+    Add a Gift List
     """
     template_name = 'giftlist/add_giftlist.html'
     model = GiftList
@@ -18,3 +19,16 @@ class AddGiftList(CreateView):
         form.instance.user = self.request.user
         return super(AddGiftList, self).form_valid(form)
 
+
+class AddItem(LoginRequiredMixin, CreateView):
+    """
+    Add an Item to Gift List
+    """
+    template_name = 'giftlist/add_item.html'
+    model = Item
+    form_class = ItemForm
+    success_url = 'giftlist/giftlist/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddItem, self).form_valid(form)
