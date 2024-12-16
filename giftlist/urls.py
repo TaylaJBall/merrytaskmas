3531c7f82802198eeb3a40a9c4fa687c6c5f83eb
Tayla Ball <tayla.ball@hotmail.com>
2024-12-16 16:37:29 +0000

    Add forms to forms.py in giftlist app

--- giftlist/urls.py
+++ giftlist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GiftList
+from .views import GiftList, AddGiftList
 
 urlpatterns = [
-    path('', GiftList.as_view(), name='giftlist')
+    path('', GiftList.as_view(), name='giftlist'),
+    path('add_giftlist/', AddGiftList.as_view(), name='add_giftlist'),
 ]
--- giftlist/views.py
+++ giftlist/views.py
@@ -1,4 +1,20 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from .models import GiftList
+from .forms import GiftListForm, ItemForm
 
 class GiftList(TemplateView):
     template_name = 'giftlist/giftlist.html'
+
+class AddGiftList(CreateView):
+    """
+    Add Giftlist
+    """
+    template_name = 'giftlist/add_giftlist.html'
+    model = GiftList
+    form_class = GiftListForm
+    success_url = 'giftlist/giftlist/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddGiftList, self).form_valid(form)
+
