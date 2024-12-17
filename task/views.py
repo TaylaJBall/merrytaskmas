e609a96b35d02628e3a8ed96224f217907662dd0
Laura Hall <laura@allthehalls.com>
2024-12-17 17:18:35 +0000

    Style: styling for task pages

--- task/views.py
+++ task/views.py
@@ -18,7 +18,7 @@ class TodoList(LoginRequiredMixin, ListView):
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
-    template_name = 'task/task.html'
+    template_name = 'task/todo_list.html'
     context_object_name = 'task'
 
 
